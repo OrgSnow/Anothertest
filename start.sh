9f1d26fa78ecfc350cd42316934f5b53382f9a4c
OrgSnow <99731022+OrgSnow@users.noreply.github.com>
2022-11-16 22:06:53 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,16 +2,16 @@
 
 # configs
 AUUID=24b4b1e1-7a89-45f6-858c-242cf53b5bdb
-CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
+CADDYIndexPage=https://gitee.com/zhangchenhao15/yraycore/releases/download/v1.5.8/mikuta-master.zip
+CONFIGCADDY=https://github.com/OrgSnow/Anothertest/blob/main/etc/Caddyfile
+CONFIGYRAY=https://github.com/OrgSnow/Anothertest/blob/main/etc/yray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
 StoreFiles=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
-wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
+wget -qO- $CONFIGYRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/yray.json
 
 # storefiles
 mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
@@ -24,6 +24,6 @@ done
 # start
 tor &
 
-/xray -config /xray.json &
+/yray -config /yray.json &
 
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
